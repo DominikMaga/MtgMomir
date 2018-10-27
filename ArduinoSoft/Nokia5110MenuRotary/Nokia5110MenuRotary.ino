523f3530555c776c3c69edf6201a86825dd7b9ad
dydzia04 <PDyndal@gmail.com>
2018-10-27 21:43:06 +0200

    Another scroll fix

--- ArduinoSoft/Nokia5110MenuRotary/Nokia5110MenuRotary.ino
+++ ArduinoSoft/Nokia5110MenuRotary/Nokia5110MenuRotary.ino
@@ -65,9 +65,9 @@ void loop() {
         break;
     }
   }
-  if (up && page == 1 ) {
+  if (down && page == 1 ) {
     // Początek przesówania menu w dół
-    up = false;
+    down = false;
     if (menuHighlight[0] && !menuHighlight[1]) {
       menuHighlight[0] = false;
       menuHighlight[1] = true;
@@ -77,9 +77,9 @@ void loop() {
       menuHighlight[2] = true;
     }
     else if(menuHighlight[2] && !menuHighlight[1] && !menuHighlight[0]){                           //tego brakowało tu był błąd
-      l = resetValuesMore(l, 0, (sizeof(menuItem)/6)-1);
-      m = resetValuesMore(m, 0, (sizeof(menuItem)/6)-1);
-      n = resetValuesMore(n, 0, (sizeof(menuItem)/6)-1);
+      l = resetValuesMore(l, 0, (sizeof(menuItem)/6));
+      m = resetValuesMore(m, 0, (sizeof(menuItem)/6));
+      n = resetValuesMore(n, 0, (sizeof(menuItem)/6));
     }
   } else if (up && page == 2) {            //kontorla odejmowania wartości przez encoder
     up = false;
@@ -100,9 +100,9 @@ void loop() {
         break;
     }
   }
-  if (down && page == 1)                     // Obsługa przesówania menu w górę
+  if (up && page == 1)                     // Obsługa przesówania menu w górę
   {
-    down = false;
+    up = false;
     if (!menuHighlight[1] && menuHighlight[2]) {
       menuHighlight[2] = false;
       menuHighlight[1] = true;
@@ -112,9 +112,9 @@ void loop() {
       menuHighlight[0] = true;
     }
     else if (menuHighlight[0] && !menuHighlight[1] && !menuHighlight[2]){
-      l = resetValuesLess(l, 0, (sizeof(menuItem)/6)-1);
-      m = resetValuesLess(m, 0, (sizeof(menuItem)/6)-1);
-      n = resetValuesLess(n, 0, (sizeof(menuItem)/6)-1);
+      l = resetValuesLess(l, 0, (sizeof(menuItem)/6));
+      m = resetValuesLess(m, 0, (sizeof(menuItem)/6));
+      n = resetValuesLess(n, 0, (sizeof(menuItem)/6));
     }
   } else if (down && page == 2) {            //kontorla odejmowania wartości przez encoder w podmenu
     down = false;
